19fb19a591466a35724cb85ae65528fe48850ca7
tantowijh <71215938+tantowijh@users.noreply.github.com>
2022-06-02 16:51:19 +0700

    Update theme-helper.php

--- theme-helper.php
+++ theme-helper.php
@@ -29,7 +29,7 @@ include_once(TRYDO_ADDONS_DIR . '/include/social-share.php');
 include_once(TRYDO_ADDONS_DIR . '/include/widgets/custom-widget-register.php');
 include_once(TRYDO_ADDONS_DIR . '/include/common-functions.php');
 include_once(TRYDO_ADDONS_DIR . '/include/allow-svg.php');
-include_once(TRYDO_ADDONS_DIR . '/include/ozc-acf.php');
+/* include_once(TRYDO_ADDONS_DIR . '/include/ozc-acf.php'); */
 
 /**
  * Load Custom Addonss
