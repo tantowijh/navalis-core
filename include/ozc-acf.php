d942ab2f38d36faf4bd7ac3529b581de9318bfbb
tantowijh <71215938+tantowijh@users.noreply.github.com>
2022-06-02 15:56:20 +0700

    Update ozc-acf.php

--- include/ozc-acf.php
+++ include/ozc-acf.php
@@ -1,46 +1,57 @@
 <?php
 
-//Save and Load ACF JSON
-define( 'MY_PLUGIN_DIR_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
-add_filter('acf/settings/save_json', 'my_acf_json_save_point');
- 
-function my_acf_json_save_point( $path ) {
-    
-    // Update path
-    $path = get_template_directory(). '/include/acf-json';
-    // Return path
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/** Start: Detect ACF Pro plugin. Include if not present. */
+if ( !class_exists('acf') ) { // if ACF Pro plugin does not currently exist
+  /** Start: Customize ACF path */
+  add_filter('acf/settings/path', 'cysp_acf_settings_path');
+  function cysp_acf_settings_path( $path ) {
+    $path = plugin_dir_path( __FILE__ ) . 'include/acf/';
     return $path;
-    
+  }
+  /** End: Customize ACF path */
+  /** Start: Customize ACF dir */
+  add_filter('acf/settings/dir', 'cysp_acf_settings_dir');
+  function cysp_acf_settings_dir( $path ) {
+    $dir = plugin_dir_url( __FILE__ ) . 'include/acf/';
+    return $dir;
+  }
+  /** End: Customize ACF path */
+  /** Start: Hide ACF field group menu item */
+  add_filter('acf/settings/show_admin', '__return_false');
+  /** End: Hide ACF field group menu item */
+  /** Start: Include ACF */
+  include_once( plugin_dir_path( __FILE__ ) . 'include/acf/acf.php' );
+  /** End: Include ACF */
+  /** Start: Create JSON save point */
+  add_filter('acf/settings/save_json', 'cysp_acf_json_save_point');
+  function cysp_acf_json_save_point( $path ) {
+    $path = plugin_dir_path( __FILE__ ) . 'include/acf-json/';
+    return $path;
+  }
+  /** End: Create JSON save point */
+  /** Start: Create JSON load point */
+  add_filter('acf/settings/load_json', 'cysp_acf_json_load_point');
+  /** End: Create JSON load point */
+  /** Start: Stop ACF upgrade notifications */
+  add_filter( 'site_transient_update_plugins', 'cysp_stop_acf_update_notifications', 11 );
+  function cysp_stop_acf_update_notifications( $value ) {
+    unset( $value->response[ plugin_dir_path( __FILE__ ) . 'include/acf/acf.php' ] );
+    return $value;
+  }
+  /** End: Stop ACF upgrade notifications */
+} else { // else ACF Pro plugin does exist
+  /** Start: Create JSON load point */
+  add_filter('acf/settings/load_json', 'cysp_acf_json_load_point');
+  /** End: Create JSON load point */
+} // end-if ACF Pro plugin does not currently exist
+/** End: Detect ACF Pro plugin. Include if not present. */
+/** Start: Function to create JSON load point */
+function cysp_acf_json_load_point( $paths ) {
+  $paths[] = plugin_dir_path( __FILE__ ) . 'include/acf-json-load';
+  return $paths;
 }
-add_filter('acf/settings/load_json', 'my_acf_json_load_point');
-/**
- * Register the path to load the ACF json files so that they are version controlled.
- * @param $paths The default relative path to the folder where ACF saves the files.
- * @return string The new relative path to the folder where we are saving the files.
- */
-function my_acf_json_load_point( $paths ) {
-   // Remove original path
-   unset( $paths[0] );// Append our new path
-   $paths[] = plugin_dir_path(). '/include/acf-json';   return $paths;
-}
-
-// Define path and URL to the ACF plugin.
-define( 'MY_ACF_PATH', get_stylesheet_directory() . '/include/acf/' );
-define( 'MY_ACF_URL', plugin_dir_url() . '/include/acf/' );
-
-// Include the ACF plugin.
-include_once( MY_ACF_PATH . 'acf.php' );
-
-// Customize the url setting to fix incorrect asset URLs.
-add_filter('acf/settings/url', 'my_acf_settings_url');
-function my_acf_settings_url( $url ) {
-    return MY_ACF_URL;
-}
-
-// (Optional) Hide the ACF admin menu item.
-add_filter('acf/settings/show_admin', 'my_acf_settings_show_admin');
-function my_acf_settings_show_admin( $show_admin ) {
-    return true;
-}
+/** End: Function to create JSON load point */
 
 ?>
